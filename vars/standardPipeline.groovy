050f0dcbb54fe7d35a482528681efc3f0b8b7119
Kai <kai.lehmann@lekatech.de>
2018-03-14 17:20:06 +0100

    erfw

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -15,7 +15,7 @@ def call(body) {
 				//sh "git ${config.repoUrl}"
 			}
 			stage ('Build') {
-				sh "echo 'building ${config.projectName} ...'"
+				echo 'building ${config.projectName} ...'
 			}
 			stage ('Tests') {
 				parallel 'static': {
